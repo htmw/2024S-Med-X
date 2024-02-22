f7795a2437fd0565d244dc7b6e4485a6b414833f
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-22 01:07:02 -0500

    working with routes on App.js and no changes on dashboard page

--- med-x/src/App.js
+++ med-x/src/App.js
@@ -1,14 +1,31 @@
 import "./App.css";
+import React from 'react';
 import "./components/navbar/navbar";
-//import NavBar from './components/navbar/navbar.js';
 import Main from "./components/main/main";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Medexer from '../../med-x/src/pages/medexer';
+import Dashboard from '../../med-x/src/pages/dashboard';
+
 
 function App() {
   return (
+    /*
     <div className="App overflow-y-scroll no-scrollbar ">
       <Main />
     </div>
+    */
+    
+      <Router>
+      <div className="App overflow-y-scroll no-scrollbar">
+        <Routes> {/* Wrap Routes around your Routes */}
+          <Route path="/dashboard" element={<Dashboard />} /> {/* Use element prop to specify the component */}
+          <Route path="/medexer" element={<Medexer />} /> {/* Use element prop to specify the component */}
+        </Routes>
+        <Main />
+      </div>
+    </Router>
+    
   );
-}
+};
 
 export default App;
--- med-x/src/pages/dashboard.js
+++ med-x/src/pages/dashboard.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import Topbar from "../topbar/topbar";
+import Topbar from '../components/topbar/topbar';
 
 export const Dashboard = () => {
   return (
+   
     <div className="flex-grow">
         <Topbar />
-        
-        
     </div>
-  )
-}
+    
+    
+   /*
+    <div className="text-green h-full p-3">
+    dashboard
+ </div>
+ */
+  );
+};
 
 export default Dashboard;
