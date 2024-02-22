915cc006dd23ba5a1d5d02e4f94fb5a03fc6dff6
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-21 23:32:13 -0500

    footer updated

--- med-x/src/components/footer/footer.js
+++ med-x/src/components/footer/footer.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <div className="w-full h-[58px] rounded-[10px] justify-start items-start gap-2.5 inline-flex">
-            <div className="grow shrink basis-0 h-[58px] p-2.5 bg-neutral-900 rounded-[10px] justify-center items-center gap-2.5 flex">
-                <div className="grow shrink basis-0 self-stretch bg-neutral-900 text-center text-zinc-300 text-opacity-30 text-base font-normal font-['Inter']">copyright Med-X AI</div>
+        <div className="grow shrink basis-0 justify-center items-center gap-2.5 flex-col p-3">
+            <div className="grow shrink basis-0 justify-center items-center gap-2.5 flex-col">
+                <div className="grow shrink basis-0 self-stretch p-3 bg-neutral-900 text-center text-zinc-300 text-opacity-30 text-base font-normal font-['Inter'] rounded-[10px]">copyright Med-X AI
+                </div>
             </div>
         </div>
-
-
 
     );
 
