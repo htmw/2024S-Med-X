5a9d17426f007e06a614537cd05dd766601ead24
PreetRai <prithviraimangya@gmail.com>
2024-02-23 23:32:46 -0500

    test init

--- Project/my-react-app/src/pages/dashboard.js
+++ Project/my-react-app/src/pages/dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div className="text-green h-full p-3">
-            dashboard
+            dashboards
          </div>
     );
 };
