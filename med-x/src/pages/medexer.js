83b4d0d4dc7f810f00814e7d77687fa2600cf658
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-23 19:04:25 -0500

    medexer small update on class

--- med-x/src/pages/medexer.js
+++ med-x/src/pages/medexer.js
@@ -59,7 +59,7 @@ const Medexer = () => {
                 <input
                     id="fileInput"
                     type="file"
-                    accept=".png,.jpeg,.svg,.txt"
+                    accept=".png,.jpeg"
                     style={{ display: 'none' }}
                     onChange={(e) => {
                         /*
@@ -81,7 +81,7 @@ const Medexer = () => {
                 <button className="px-5 py-2.5 bg-white bg-opacity-80 rounded-[10px] justify-start items-start gap-2.5 inline-flex hover:bg-green-600 active:bg-green-700 focus:ring focus:ring-gray-700" onClick={handlingFileUpload}>
                     <div className="text-neutral-900 text-2xl font-normal font-['Inter']">Browse File</div>
                 </button>
-                <div className="text-zinc-300 text-opacity-30 text-base font-normal font-['Inter']">Supported File formats: .png, .jpeg, .svg</div>
+                <div className="text-zinc-300 text-opacity-30 text-base font-normal font-['Inter']">Supported File formats: .png, .jpeg</div>
             </div>
         </div>
     )
