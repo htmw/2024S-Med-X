83441ce2eff5ad3a1bc39d3a64f2819ecc7e7510
PreetRai <prithviraimangya@gmail.com>
2024-02-26 17:04:18 -0500

    merge test to Prithvi

--- Project/my-react-app/src/pages/dashboard.js
+++ Project/my-react-app/src/pages/dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div className="text-white p-3 h-full flex-col justify-center items-center gap-2.5 inline-flex">
-            dashboards
+            dashboards...
          </div>
     );
 };
