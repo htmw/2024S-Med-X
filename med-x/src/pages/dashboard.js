cdbb277155d977264f6e56750bec8f91f3edef03
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-26 14:20:35 -0500

    dasboard upadtes, not found page created, report page updated

--- med-x/src/pages/dashboard.js
+++ med-x/src/pages/dashboard.js
@@ -3,18 +3,11 @@ import React from 'react'
 export const Dashboard = () => {
   return (
 
-    <div className="flex-grow h-full">
+    <div className="text-white p-3 h-full flex-col justify-center items-center gap-2.5 inline-flex">
       {/* <Topbar /> */}
-      Hi Ronaldo!1235567778
+      Welcome to Med-X Dashboard
 
     </div>
-
-
-    /*
-     <div className="text-green h-full p-3">
-     dashboard
-  </div>
-  */
   );
 };
 
