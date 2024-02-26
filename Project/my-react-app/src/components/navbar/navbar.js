4889749f857ee6f09ec4463cbe8eafdc8b360f0b
PreetRai <prithviraimangya@gmail.com>
2024-02-26 01:20:09 -0500

    updated navbar design

--- Project/my-react-app/src/components/navbar/navbar.js
+++ Project/my-react-app/src/components/navbar/navbar.js
@@ -5,59 +5,36 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div
-            className="h-full w-fit p-[35px] bg-primary rounded-tr-[20px] rounded-br-[20px]  flex-col justify-start items-start gap-2.5 inline-flex">
-            <div className="self-stretch h-full flex-col justify-between items-center flex">
-                <div className="p-[15px] flex-col justify-start items-center gap-2.5 flex">
+            className="h-full w-fit p-5 bg-primary  flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="self-stretch h-full flex-col justify-between items-center flex gap-9">
+                <div className=" flex-col justify-start items-center gap-2.5 flex">
                     <div
-                        className="w-[200px] h-[200px] shadow justify-center items-center inline-flex">
+                        className="w-[180px] h-[180px] justify-center items-center inline-flex">
                         <img className="" src={Logo}/>
                     </div>
                 </div>
                 <div
-                    className="self-stretch grow shrink basis-0 flex-col justify-between items-center flex">
-                    <div className="w-px h-px relative"/>
+                    className="self-stretch grow shrink basis-0 flex-col justify-start items-center flex ">
+                   
                     <div
-                        className="self-stretch h-[210px] flex-col justify-between items-center flex">
-                        <div className="w-[230px] justify-start items-center gap-5 inline-flex">
-                            <div className="w-[25px] h-[25px] relative">
+                        className="self-stretch flex-col justify-between items-start flex gap-5">
+                        <div className="w-fit justify-start items-center gap-3 inline-flex">
+                            <div className="w-[20px] h-[20px] relative">
                                 <img src={vector} className=''/></div>
                             <div className="text-white text-base font-normal font-['Inter']"><Link to="/dashboard">Dashboard</Link></div>
                         </div>
-                        <div className="w-[230px] justify-start items-center gap-5 inline-flex">
-                        <div className="w-[25px] h-[25px] relative">
+                        <div className="w-fit justify-start items-center gap-3 inline-flex">
+                        <div className="w-[20px] h-[20px] relative">
                                 <img src={vector} className=''/></div>
                             <div className="text-white text-base font-normal font-['Inter']"><Link to="/medexer">Medexer</Link></div>
                         </div>
-                        <div className="w-[230px] justify-start items-center gap-5 inline-flex">
-                        <div className="w-[25px] h-[25px] relative">
+                        <div className="w-fit justify-start items-center gap-3 inline-flex">
+                        <div className="w-[20px] h-[20px] relative">
                                 <img src={vector} className=''/></div>
                             <div className="text-white text-base font-normal font-['Inter']"><Link to="/report">Report</Link></div>
                         </div>
-                        <div className="w-[230px] justify-start items-center gap-5 hidden">
-                        <div className="w-[25px] h-[25px] relative">
-                                <img src={vector} className=''/></div>
-                            <div className="text-white text-base font-normal font-['Inter']">Connect</div>
-                        </div>
                     </div>
-                    <div
-                        className="self-stretch h-[155px] flex-col justify-between items-center hidden">
-                        <div className="w-[230px] justify-start items-center gap-5 inline-flex">
-                        <div className="w-[25px] h-[25px] relative">
-                                <img src={vector} className=''/></div>
-                            <div className="text-white text-base font-normal font-['Inter']">Settings</div>
-                        </div>
-                        <div className="w-[230px] justify-start items-center gap-5 inline-flex">
-                        <div className="w-[25px] h-[25px] relative">
-                                <img src={vector} className=''/></div>
-                            <div className="text-white text-base font-normal font-['Inter']">About us</div>
-                        </div>
-                        <div className="w-[230px] justify-start items-center gap-5 inline-flex">
-                        <div className="w-[25px] h-[25px] relative">
-                                <img src={vector} className=''/></div>
-                            <div className="text-white text-base font-normal font-['Inter']">Terms and Condition</div>
-                        </div>
-                    </div>
-                    <div className="w-px h-px relative"/>
+                  
                 </div>
             </div>
         </div>
