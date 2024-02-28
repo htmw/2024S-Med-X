477c7c6400ab13d029f6e843aba6e44a529be09e
PreetRai <prithviraimangya@gmail.com>
2024-02-28 17:09:11 -0500

    updated drag and drop logic

--- Project/my-react-app/src/pages/medexer.js
+++ Project/my-react-app/src/pages/medexer.js
@@ -1,92 +1,108 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react';
 import Uploadimg from "../components/img/upload.png";
 
 const Medexer = () => {
-    const [isDraggingOver, setIsDragginOver] = useState(false);
+    const [isDraggingOver, setIsDraggingOver] = useState(false);
     const [previewFile, setPreviewFile] = useState(null);
 
-    const handlingFileUpload = () => {
+    const handleFileUpload = () => {
         const fileInput = document.getElementById('fileInput');
         fileInput.click();
-    }
-
-    const handlingDragOver = (e) => {
-        e.preventDefault();
-        setIsDragginOver(true);
-    };
-    const handlingDragLeave = (e) => {
-        e.preventDefault();
-        setIsDragginOver(false);
     };
 
-    const handlingDrop = (e) => {
+    const handleDragOver = (e) => {
         e.preventDefault();
-        setIsDragginOver(false);
+        setIsDraggingOver(true);
+    };
 
-        //logic for drag and drop file
+    const handleDragLeave = (e) => {
+        e.preventDefault();
+        setIsDraggingOver(false);
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setIsDraggingOver(false);
+
         const droppedFiles = Array.from(e.dataTransfer.files);
         const file = droppedFiles[0]; // Assuming only one file is dropped
 
         const reader = new FileReader();
         reader.onload = (event) => {
-            setPreviewFile({
-                name: file.name,
-                type: file.type,
-                size: file.size,
-                dataURL: event.target.result
-            });
+            setPreviewFile(
+                {name: file.name, type: file.type, size: file.size, dataURL: event.target.result}
+            );
         };
         reader.readAsDataURL(file);
-    }
+    };
 
     const deletePreviewFile = () => {
         setPreviewFile(null);
-    }
+    };
 
     return (
-        <div className={`text-green p-3 h-full flex-col justify-start items-start gap2.5 inline-flex ${isDraggingOver ? 'border-4 border-blue-500' : ''}`}
-            onDragOver={handlingDragOver}
-            onDrop={handlingDrop}
-            onDragLeave={handlingDragLeave}
-        >
-            <div className="self-stretch grow shrink basis-0 p-2.5 rounded-[10px] border-dotted border-2 border-zinc-300 border-opacity-30 flex-col justify-center items-center gap-5 flex">
-                <div className="w-[200px] h-[200px] relative bg-black bg-opacity-0">
-                    {previewFile && (
-                        <div>
-                            <img src={previewFile.dataURL} alt="Preview" className="w-full h-full object-cover" />
-                            <button className="px-5 py-2.5 bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex hover:bg-green-600 active:bg-green-700 focus:ring focus:ring-gray-700" onClick={deletePreviewFile}>Delete File</button>
+        <div className='w-full h-full flex flex-col'>
+            {
+                previewFile
+                    ? (<div
+                        id="Preview"
+                        className=" p-3 h-full flex-col justify-start items-start gap2.5 inline-flex ">
+                        <div
+                            className="flex-col justify-center items-start gap-5 flex w-[200px] h-[300px]">
+
+                            <img
+                                src={previewFile.dataURL}                                alt="Preview"
+                                className="w-full h-full object-cover bg-white rounded-[10px]"/>
+</div>
+                            <button
+                                className="px-5 py-2.5 bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex hover:bg-green-600 active:bg-green-700 focus:ring focus:ring-gray-700"
+                                onClick={deletePreviewFile}>Delete File</button>
                         </div>
-                    )}
-                </div>
-                <img className="" src={Uploadimg} alt=''/>
-
-                <div className="text-zinc-300 text-opacity-30 text-lg font-normal font-['Inter']">Drag & Drop to Upload Chest X-ray </div>
-                <input
-                    id="fileInput"
-                    type="file"
-                    style={{ display: 'none' }}
-                    onChange={(e) => {
-                        const file = e.target.files[0];
-
-                        const reader = new FileReader();
-                        reader.onload = (event) => {
-                            setPreviewFile({
-                                name: file.name,
-                                type: file.type,
-                                size: file.size,
-                                dataURL: event.target.result
-                            });
-                        };
-                        reader.readAsDataURL(file);
-                    }}
-                />
-                <button className="px-5 py-2.5 bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex hover:bg-green-600 active:bg-green-700 focus:ring focus:ring-gray-700" onClick={handlingFileUpload}>
-                    <div className="text-neutral-900 font-normal font-['Inter']">Browse File</div>
-                </button>
-                <div className="text-zinc-300 text-opacity-30 text-base font-normal font-['Inter']">Supported File formats: .png, .jpeg</div>
-            </div>
+                        ) : (
+                        <div
+                            id="DragNDrop"
+                            className={`text-green p-3 h-full flex-col justify-start items-start gap2.5 inline-flex ${isDraggingOver
+                                ? 'border-4 border-blue-500'
+                                : ''}`}
+                            onDragOver={handleDragOver}
+                            onDrop={handleDrop}
+                            onDragLeave={handleDragLeave}>
+                            <div
+                                className="self-stretch grow shrink basis-0 p-2.5 rounded-[10px] border-dotted border-2 border-zinc-300 border-opacity-30 flex-col justify-center items-center gap-5 flex">
+                                <img className="" src={Uploadimg} alt=''/>
+                                <div
+                                    className="text-zinc-300 text-opacity-30 text-lg font-normal font-['Inter']">Drag & Drop to Upload Chest X-ray
+                                </div>
+                                <input
+                                    id="fileInput"
+                                    type="file"
+                                    style={{
+                                        display: 'none'
+                                    }}
+                                    onChange={(e) => {
+                                        const file = e
+                                            .target
+                                            .files[0];
+                                        const reader = new FileReader();
+                                        reader.onload = (event) => {
+                                            setPreviewFile(
+                                                {name: file.name, type: file.type, size: file.size, dataURL: event.target.result}
+                                            );
+                                        };
+                                        reader.readAsDataURL(file);
+                                    }}/>
+                                <button
+                                    className="px-5 py-2.5 bg-white bg-opacity-80 rounded-[5px] justify-start items-start gap-2.5 inline-flex hover:bg-green-600 active:bg-green-700 focus:ring focus:ring-gray-700"
+                                    onClick={handleFileUpload}>
+                                    <div className="text-neutral-900 font-normal font-['Inter']">Browse File</div>
+                                </button>
+                                <div
+                                    className="text-zinc-300 text-opacity-30 text-base font-normal font-['Inter']">Supported File formats: .png, .jpeg</div>
+                            </div>
+                        </div>
+                        )}
         </div>
-    )
-}
+    );
+};
 
 export default Medexer;
