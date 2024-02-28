4179cfb74d1fb1383565af75efd435be70b8e338
PreetRai <prithviraimangya@gmail.com>
2024-02-28 00:08:29 -0500

    copied project into my branch

---
+++ Project/my-react-app/src/components/navbar/navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import vector from '../img/icons/Vector.png';
+
+import Medexericon from '../img/icons/medex-w.svg';
+import Dashicon from '../img/icons/dashcube.svg';
+import Reporticon from '../img/icons/report.svg';
+import Logo from '../img/Logo.png'
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+    return (
+        <div
+            className="h-full w-fit p-5 bg-primary  flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="self-stretch h-full flex-col justify-between items-center flex gap-9">
+                <div className=" flex-col justify-start items-center gap-2.5 flex">
+                    <div
+                        className="w-[180px] h-[180px] justify-center items-center inline-flex">
+                        <img className="" src={Logo}/>
+                    </div>
+                </div>
+                <div
+                    className="self-stretch grow shrink basis-0 flex-col justify-start items-center flex p-6 ">
+                   
+                    <div
+                        className="self-stretch flex-col justify-between items-start flex gap-5">
+                        <div className="w-fit justify-start items-center gap-3 inline-flex">
+                            <div className="w-[20px] h-[20px] relative">
+                                <img src={Dashicon} className=''/></div>
+                            <div className="text-white text-base font-normal font-['Inter']"><Link to="/">Dashboard</Link></div>
+                        </div>
+                        <div className="w-fit justify-start items-center gap-3 inline-flex">
+                        <div className="w-[20px] h-[20px] relative">
+                                <img src={Medexericon} className=''/></div>
+                            <div className="text-white text-base font-normal font-['Inter']"><Link to="/medexer">Medexer</Link></div>
+                        </div>
+                        <div className="w-fit justify-start items-center gap-3 inline-flex">
+                        <div className="w-[20px] h-[20px] relative">
+                                <img src={Reporticon} className=''/></div>
+                            <div className="text-white text-base font-normal font-['Inter']"><Link to="/report">Report</Link></div>
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
