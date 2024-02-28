6eb53262d505bcccb173b765b825481430d39311
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-28 17:04:58 -0500

    no change

--- Project/my-react-app/src/components/profile/profile.js
+++ Project/my-react-app/src/components/profile/profile.js
@@ -7,7 +7,7 @@ const Profile = () => {
         <div className="flex justify-start items-center gap-2.5 h-full rounded-[5px] bg-primary  p-2.5 ">
             <div className="flex justify-start items-center gap-2.5 flex-grow flex-shrink flex-basis-0 self-stretch">
                 <div className=" bg-opacity-30 rounded-full shadow-inner overflow-hidden h-full w-[45px]">
-                    <img src={ProfileIcon} className="object-cover w-full h-full bg-blend-overlay bg-primary" alt="Profile" />
+                    <img src={ProfileIcon} className="object-cover w-full h-full bg-blend-overlay bg-primary" alt="Profile " />
                 </div>
                 <div />
                 <img src={ChevronIcon} className="h-5 w-5" alt="Chevron" />
