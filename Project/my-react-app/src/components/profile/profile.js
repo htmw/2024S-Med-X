b584eefe45ae7b6a37cbf48d3ee2e7ae6b6b4d43
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-29 01:17:12 -0500

    small changes on footer, notification, profile, and searchbar.js

--- Project/my-react-app/src/components/profile/profile.js
+++ Project/my-react-app/src/components/profile/profile.js
@@ -4,7 +4,7 @@ import ChevronIcon from '../img/icons/chevron.svg';
 
 const Profile = () => {
     return (
-        <div className="flex justify-start items-center gap-2.5 h-full rounded-[5px] bg-primary  p-2.5 ">
+        <div className="flex justify-start items-center gap-2.5 h-full rounded-full bg-primary  p-2.5 ">
             <div className="flex justify-start items-center gap-2.5 flex-grow flex-shrink flex-basis-0 self-stretch">
                 <div className=" bg-opacity-30 rounded-full shadow-inner overflow-hidden h-full w-[45px]">
                     <img src={ProfileIcon} className="object-cover w-full h-full bg-blend-overlay bg-primary" alt="Profile " />
--- Project/my-react-app/src/components/searchbar/searchbar.js
+++ Project/my-react-app/src/components/searchbar/searchbar.js
@@ -16,25 +16,27 @@ const Searchbar = () => {
         setShowSearchOptions(!showSearchOptions);
     };
     return (
-        
-            <div className="flex flex-col justify-start items-start gap-2.5 flex-grow flex-shrink flex-basis-0 self-stretch">
-                <div className="inline-flex justify-between items-center p-5 bg-neutral-900 rounded-[5px] h-full w-full">
-                    <input
-                        ref={searchInputRef}
-                        type="text"
-                        placeholder="Search"
-                        className="text-zinc-300 w-full text-opacity-30 text-base font-normal font-['Inter'] bg-transparent border-none outline-none focus:placeholder:text-primary"
-                    />
-                    <div className="relative w-[25px] h-[25px]" onClick={handleSearchIconClick}>
-                        <img src={SearchIcon} alt="Search" />
+        <form className="flex flex-col justify-start items-start gap-2.5 flex-grow flex-shrink flex-basis-0 self-stretch">
+            <div className="inline-flex justify-between items-center p-5 bg-neutral-900 rounded-full h-full w-full ">
+                <input
+                    ref={searchInputRef}
+                    type="text"
+                    placeholder="Search"
+                    className="text-zinc-300 w-full text-opacity-30 text-base font-normal font-['Inter'] bg-transparent border-none outline-none  focus:placeholder:text-primary"
+                />
+                <button className="relative w-[25px] h-[25px] rounded-full" onClick={handleSearchIconClick}>
+                    <img src={SearchIcon} alt="Search" />
+                </button>
+                
+                {/*
+                {showSearchOptions && (
+                    <div className="absolute top-[calc(100%+5px)] left-0 w-full bg-slate-900 rounded-xl flex-col">
+                    
                     </div>
-                    {showSearchOptions && (
-                        <div className="absolute top-[calc(100%+5px)] left-0 w-full bg-white rounded-md shadow-lg">
-                            {/* Render your search options here */}
-                        </div>
-                    )}
-                </div>
+                )}
+                */}
             </div>
+        </form>
     )
 }
 
