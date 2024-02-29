d85049d6b3e46d54646d69d710a4aa46c06b0141
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-02-29 01:56:28 -0500

    Revert "small changes on footer, notification, profile, and searchbar.js"
    This reverts commit b584eefe45ae7b6a37cbf48d3ee2e7ae6b6b4d43. Going back to last succesfull comit

--- Project/my-react-app/src/components/notification/notification.js
+++ Project/my-react-app/src/components/notification/notification.js
@@ -3,7 +3,7 @@ import NotificationIcon from '../img/icons/nbell.png';
 
 const Notification = () => {
   return (
-        <div className="flex justify-center items-center gap-2.5 bg-neutral-900 rounded-full h-full p-2.5">
+        <div className="flex justify-center items-center gap-2.5 bg-neutral-900 rounded-[5px] h-full p-2.5">
                 <div className="inline-flex justify-center items-center w-[50px] h-[50px]">
                     <img src={NotificationIcon} alt="Notification" />
                 </div>
