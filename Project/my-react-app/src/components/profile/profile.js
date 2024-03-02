b7b52125e113a04b11fa2e182e1e27a7bf0831fd
Ronaldo Simbana <ronaldoismael15@hotmail.com>
2024-03-02 01:18:34 -0500

    small update on profile.js

--- Project/my-react-app/src/components/profile/profile.js
+++ Project/my-react-app/src/components/profile/profile.js
@@ -1,16 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ProfileIcon from '../img/Profile.png';
 import { FaChevronDown } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 const Profile = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsDropdownOpen(false);
+        }
+    };
+    //event listener to close list of options from Chevronicon when clicking somewhere else and not just on the Chevronicon
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+
+
     return (
-        <div className="relative h-full">
+        <div className="relative h-full" ref={dropdownRef}>
             <div className="flex justify-start items-center gap-2.5 h-full rounded-full bg-primary p-2.5 ">
                 <div className="bg-opacity-30 rounded-full shadow-inner overflow-hidden h-full w-[45px]">
                     <img className="object-cover w-full h-full bg-blend-overlay bg-primary" src={ProfileIcon} alt="profile" />
@@ -23,8 +39,8 @@ const Profile = () => {
                     {/* Dropdown content here */}
                     <ul >
                         <li className="py-2 px-4 hover:bg-secondary rounded-lg text-white">
-                          <Link >Option 1</Link>
-                            </li>
+                            <Link >Option 1</Link>
+                        </li>
                         <li className="py-2 px-4 hover:bg-secondary rounded-lg text-white">   <Link >Option 2</Link></li>
                         <li className="py-2 px-4 hover:bg-secondary rounded-lg text-white">   <Link >Option 3</Link></li>
                     </ul>
