5b36ad77e0deef758a3279834b48ac4427ad3a42
Thermax1 <am15943n@pace.edu>
2024-03-12 19:57:28 -0400

    added flask server

---
+++ flask-server/readme.md
@@ -0,0 +1,77 @@
+# Backend Flask API Setup on Docker
+
+Below are instructions on how to setup backend Docker image and run Docker container.
+
+## Setting Up Docker Image
+
+1. Make sure you are in flask-server directory and run the below command to create image.
+
+    ```
+    docker build -t flask-app .
+    ```
+
+## Running the container from the image
+
+2. Run the below command to create container.
+
+    ```
+    docker run -p 5001:5000 flask-app
+    ```
+
+# Backend Flask API Setup on Local Machine
+
+Below are instructions on how to set up your Python environment and install necessary packages.
+
+## Setting Up Python Virtual Environment
+
+### Windows
+
+1. Open Command Prompt.
+2. Navigate flask-server directory using.
+3. Run the following command to create a virtual environment named `env`:
+
+    ```
+    python -m venv env
+    ```
+
+4. Activate the virtual environment by running:
+
+    ```
+    .\env\Scripts\activate
+    ```
+
+### Mac
+
+1. Open Terminal.
+2. Navigate to flask-server directory.
+3. Run the following command to create a virtual environment named `env`:
+
+    ```
+    python3 -m venv env
+    ```
+
+4. Activate the virtual environment by running:
+
+    ```
+    source env/bin/activate
+    ```
+
+## Installing Packages Using requirements.txt
+
+After activating your virtual environment, you can install all the required packages using the `requirements.txt` file.
+
+1. Ensure you're in the directory of flask-server where `requirements.txt` is located.
+2. Run the following command:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+## Running Flask App
+
+After installing required packages, use the following command to run the flask server.
+
+```
+python server.py
+```
+
---
+++ flask-server/server.py
@@ -0,0 +1,79 @@
+#Resources:
+#https://www.youtube.com/watch?v=7LNl2JlZKHA (tutorial to set up simple API in React using Flask)
+#https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/ (Tutorial to install Python environment in Windows)
+#https://www.geeksforgeeks.org/how-to-install-flask-in-windows/ (tutorial to install Flask in Windows)
+#Before running React app, test API at localhost:5000/members. 
+#How to run this server:
+# run "cd .ven\" AND "Scripts\activate" (Windows) or "source ven/bin/activate" (Mac)
+# run "pip install flask" (Windows) or "pip3 install flask" (Mac)
+# run "python server.py" (Windows) or "python3 server.py" (Mac)
+#
+#to stop server, run "deactivate"
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS
+from tensorflow.keras.models import load_model
+from predictions import get_finding
+
+# Load the model
+model = load_model('dicom_images.keras')
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/predict', methods=['GET', 'OPTIONS'])
+
+def predict():
+    try:
+        # Get image URL from request arguments
+        image_url = request.args.get('image_url')
+
+        # Download image
+        print("Image URL:", image_url)
+        response = requests.get(image_url, stream=True)
+
+        # Check if image download is successful
+        if response.status_code == 200:
+            # Get content type from response header
+            content_type = response.headers.get('Content-Type')
+
+            # Check content type and determine file extension
+            if content_type == "image/jpeg":
+                file_extension = "jpg"
+            elif content_type == "image/png":
+                file_extension = "png"
+            elif content_type == "image/dicom":
+                file_extension = "dicom"
+            else:
+                raise ValueError(f"Unsupported image format: {content_type}")
+
+            # Save image with appropriate filename
+            filename = f"xray_image.{file_extension}"
+            with open(filename, 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            print("Image downloaded successfully!")
+        else:
+            print("Failed to download image. Status code:", response.status_code)
+            return jsonify({'error': 'Failed to download image'}), 500
+
+        # Call prediction function with appropriate filename
+        prediction = get_finding(model, filename)
+
+        # Return prediction result
+        return jsonify({'prediction': prediction}), 200
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return jsonify({'error': str(e)}), 500
+
+
+#Members API Route
+@app.route("/members")
+
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]} #dictionary (key-value pair) with array as value
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
